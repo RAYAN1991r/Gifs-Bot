b5108349e41a588d0ec1030f733d58d3f79170e8
Hevar525 <83186404+Hevar525@users.noreply.github.com>
2022-06-20 23:06:03 +0300

    Create randompp.js

---
+++ commands/randompp.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+exports.run = (client, message) => {
+  const Embed = new Discord.MessageEmbed()
+  .setColor("RANDOM")
+  .setTimestamp()
+  .setFooter("VİCTORY")
+  .setImage(client.users.cache.random().displayAvatarURL({ display: true, dynamic: true }))
+  message.channel.send(Embed)
+}
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["random-pp"]
+};
+
+exports.help = {
+  name: 'randompp'
+};
